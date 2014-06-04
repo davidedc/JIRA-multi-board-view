60bb01eb7d9aed91e200976800b7556903ff7ab1
Davide Della Casa <davidedc@gmail.com>
2014-06-04 16:28:44 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,11 @@
+Will show issues spread across multiple boards (draft alert: this is work in progress).
+
+Could be useful for example to show how Epics are doing across several teams/projects.
+
+Setup:
+- have Python installed
+- install the "jira" library (usually "pip install jira")
+
+Usage:
+- python vizit portNumber jiraServerURL login password
+- then go to http://localhost:portNumber/index.html
---
+++ vizit.py
@@ -0,0 +1,74 @@
+
+# Usage:
+#  python vizit portNumber jiraServerURL login password
+#  then go to http://localhost:portNumber/index.html
+
+# for the server part
+import SocketServer
+import SimpleHTTPServer
+import urllib
+
+# for grokking the arguments coming from the command line
+import sys
+
+# for sending the results as JSON to the page
+import json
+
+# to access JIRA
+from jira.client import JIRA
+
+PORT = int(sys.argv[1])
+
+
+# By default, the client will connect to a JIRA instance started from the Atlassian Plugin SDK
+# (see https://developer.atlassian.com/display/DOCS/Installing+the+Atlassian+Plugin+SDK for details).
+# Override this with the options parameter.
+options = {
+	'server': sys.argv[2]
+}
+
+jira = JIRA(options, basic_auth=(sys.argv[3], sys.argv[4]))
+
+
+
+class Proxy(SimpleHTTPServer.SimpleHTTPRequestHandler):
+	def do_GET(self):
+		theFile = self.path
+		theFile = theFile[1:] # first character is a slash, we take that away
+		print "requested ", self.path
+		if "favicon" in theFile:
+			print ""
+		elif "search" in theFile:
+			# extract the callback name
+			callbackName = theFile.split("?callback=",1)[1].split("&",1)[0]
+
+			#This URL will trigger our sample function and send what it returns back to the browser
+			self.send_response(200)
+			self.send_header('Content-type','application/json')
+			self.end_headers()
+
+			searchResults = jira.search_issues('project = SIO AND "Epic Link" = SRM-488', maxResults=500)
+			issuesData = [[ issue.fields.project.key, issue.key, issue.fields.summary, issue.fields.status.name] for issue in searchResults]
+
+			JSONOfIssues = json.dumps(issuesData)
+
+			print "json: " +  JSONOfIssues
+
+			self.wfile.write(callbackName + "({result: " + JSONOfIssues + "})")
+
+		elif "index" in theFile:
+			# next three lines only needed for Firefox
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			self.copyfile(urllib.urlopen(theFile), self.wfile)
+		else:
+			# send empty response
+			self.send_response(200)
+			self.send_header('Content-type','text/html')
+			self.end_headers()
+			self.wfile.write("")
+
+httpd = SocketServer.ForkingTCPServer(('', PORT), Proxy)
+print "serving at port", PORT
+httpd.serve_forever()
