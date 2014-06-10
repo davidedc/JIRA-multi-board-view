f2484293b165a108ae17193247c9770b2d7de2f7
Tomasz Kustrzynski <tomasz.kustrzynski@gmail.com>
2014-06-10 11:36:30 +0100

    Fix urls

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'multi_board_view.views.main', name='main'),
-    url(r'^search/', 'multi_board_view.views.search', name='search'),
+    url(r'^$', 'JIRA_multi_board_view.views.main', name='main'),
+    url(r'^search/', 'JIRA_multi_board_view.views.search', name='search'),
 )
